8ed35f1e319a013844b34643c8c0d66709566978
almazka <almazka@flylady.su>
2016-11-10 11:36:34 +0600

    Added improvements for regexp 2B task

--- src/getCorrectName.js
+++ src/getCorrectName.js
@@ -1,20 +1,22 @@
 import _ from 'lodash';
+import getTitleCase from './getTitleCase';
 
 export default function getCorrectName(query) {
 	const fullname = ( query.fullname ) ? query.fullname : false;
-	const reg = /^(?:\s+)?([А-Яа-яЁёó\w-\'.]*)(?: +)?([А-Яа-яЁёó\w-\'.]*)(?: +)?([А-Яа-яЁёó\w-\'.]*)?/;
+	const reg = /^(?:\s+)?([А-Яа-яÀ-ÿЁё\w-\'.]*)(?: +)?([А-Яа-яÀ-ÿЁё\w-\'.]*)(?: +)?([А-Яа-яÀ-ÿЁё\w-\'.]*)?/;
 	const regExclude = /[\d_\/]+/;
+
 	if (fullname && !fullname.match(regExclude)) {
 		const nm = fullname.match(reg);
 
 		const count = _.compact(_.split(nm['input'], / +/g)).length;
 
 		if (count == 1) {
-			return `${_.upperFirst(_.lowerCase(nm[1]))}`;
+			return getTitleCase(`${nm[1]}`);
 		} else if (count == 2) {
-			return `${_.upperFirst(nm[2])} ${_.upperCase(nm[1].slice(0,1))}.`;
+			return getTitleCase(`${nm[2]} ${nm[1].slice(0,1)}.`);
 		} else if (count == 3) {
-			return `${_.upperFirst(_.lowerCase(nm[3]))} ${_.upperCase(nm[1].slice(0,1))}. ${_.upperCase(nm[2].slice(0,1))}.`;
+			return getTitleCase(`${nm[3]} ${nm[1].slice(0,1)}. ${nm[2].slice(0,1)}.`);
 		} else {
 			return 'Invalid fullname';
 		}
