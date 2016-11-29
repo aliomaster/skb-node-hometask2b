1831b64b0b4e61ba10b4bfd209cf024181229864
Alio Webdeveloper <aliowebdeveloper@gmail.com>
2016-11-29 21:17:31 +0600

    Task 3C completed

---
+++ src/config/index.js
@@ -0,0 +1,9 @@
+global.__DEV__ = true;
+
+export default {
+	name: 'Fat Poke',
+	port: 3000,
+	pokeapi: {
+		url: 'http://pokeapi.co/api/v2/pokemon'
+	}
+}
--- src/index.js
+++ src/index.js
@@ -1,4 +1,99 @@
-const App = require('./app');
+import bunyan from 'bunyan';
+import cors from 'cors';
+import express from 'express';
+import Promise from 'bluebird';
+import fetch from 'isomorphic-fetch';
+import _ from 'lodash';
+import config from './config';
+import pokemons from './pokemons';
+import getApi from './api';
 
-const app = new App();
-app.run(8085, 'App running in port 8085!');
+const pokemonFields = ['id', 'name', 'height', 'weight'];
+const metrica = ['fat', 'angular', 'heavy', 'light', 'huge', 'micro'];
+
+class App {
+	constructor(params) {
+		Object.assign(this, params);
+		if(!this.log) this.log = this.getLogger();
+		this.init();
+	}
+
+	getLogger(params) {
+		return bunyan.createLogger(Object.assign({
+			name: 'task3c',
+			src: __DEV__,
+			level: 'trace'
+		}), params);
+	}
+
+	async getPokemons(url, i=0 ) {
+		this.log.trace("getPokemons", url, i);
+		const response = await fetch(url);
+		const page = await response.json();
+		const pokemons = page.results;
+
+		if(__DEV__ && i >= 1) {
+			return pokemons;
+		}
+		if(page.next) {
+			const pokemonsOffset = await this.getPokemons(page.next, i + 1);
+			return pokemons.concat(pokemonsOffset);
+		}
+		return pokemons;
+	}
+
+	async getPokemon(url) {
+		this.log.trace('getPokemon', url);
+		const response = await fetch(url);
+		const pokemon = await response.json();
+		return pokemon;
+	}
+
+	initPokemons() {
+		return {
+			run: async () => {
+				try {
+					const pokemonsInfo = await this.getPokemons(this.config.pokeapi.url);
+					const pokemonsPromise = pokemonsInfo.map((info) => {
+						return this.getPokemon(info.url);
+					})
+					const pokemonsFull = await Promise.all(pokemonsPromise);
+					const pokemons = pokemonsFull.map(pokemon => {
+						let pokemonObj = {};
+						pokemonFields.forEach(field => {
+							pokemonObj[field] = pokemon[field];
+						});
+						return pokemonObj;
+					});
+
+					return pokemons;
+				} catch (e) {
+						this.log.error(e);
+				}
+			}
+		}
+	}
+
+	init() {
+		this.app = express();
+		this.app.use(cors());
+		this.useRoute();
+	}
+
+	useRoute() {
+		const api = getApi(this);
+		this.app.use('/task3C', api);
+	}
+
+	run() {
+		return new Promise((resolve) => {
+			this.app.listen(this.config.port, () => {
+				this.log.info(`App "${this.config.name}" running on port ${this.config.port}`);
+				resolve(this);
+			});
+		});
+	}
+}
+
+const app = new App({config, pokemons, metrica});
+app.run();
